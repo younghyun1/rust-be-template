5ac1b6b0d4666ad8734a61410b5311579362d70e
Ji Younghyun <younghyun1@gmail.com>
2025-01-21 20:15:23 +0900

    feature: more consistent structured logging; info and up

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 use init::server_init::server_init_proc;
-use tracing::info;
+use tracing::{info, level_filters};
+use tracing_subscriber::layer::SubscriberExt;
+use tracing_subscriber::util::SubscriberInitExt;
+use tracing_subscriber::Layer;
 
 // modules tree
 pub mod domain {
@@ -54,7 +57,40 @@ pub mod util {
 #[tokio::main(flavor = "multi_thread")]
 async fn main() -> anyhow::Result<()> {
     let start = tokio::time::Instant::now();
-    tracing_subscriber::fmt().json().with_target(true).init();
+
+    if std::env::var("IS_AWS").is_err() {
+        println!(r#"{{"message": "env. variable IS_AWS missing, assuming .env exists..."}}"#);
+        dotenvy::dotenv().map_err(|e| anyhow::anyhow!("Failed to load .env: {}", e))?;
+    }
+
+    let app_name_version = std::env::var("APP_NAME_VERSION")
+        .map_err(|e| anyhow::anyhow!("Failed to get APP_NAME_VERSION: {}", e))?;
+
+    let filename = format!("{}", app_name_version);
+
+    let file_appender =
+        tracing_appender::rolling::daily(format!("./log/{}", app_name_version), filename);
+    let (non_blocking_file, _guard) = tracing_appender::non_blocking(file_appender);
+
+    // Create a console layer
+    let console_layer = tracing_subscriber::fmt::layer()
+        // .json()
+        .with_ansi(true)
+        .with_target(true)
+        .with_filter(level_filters::LevelFilter::INFO);
+
+    // Create a file layer
+    let file_layer = tracing_subscriber::fmt::layer()
+        .with_target(true)
+        .json()
+        .with_writer(non_blocking_file)
+        .with_filter(level_filters::LevelFilter::INFO);
+
+    // Build a subscriber that combines both layers
+    tracing_subscriber::registry()
+        .with(console_layer)
+        .with(file_layer)
+        .init();
 
     info!("Initializing server...");
     server_init_proc(start).await?;
