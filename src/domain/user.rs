4f2cce667d4499421a9341fa41cdb75c35a7441d
Ji Younghyun <younghyun1@gmail.com>
2025-01-24 12:16:04 +0900

    feature: new tables for tokens applied to user domain

--- src/domain/user.rs
+++ src/domain/user.rs
@@ -10,6 +10,38 @@ table! {
         user_password_hash -> Varchar,
         user_created_at -> Timestamptz,
         user_updated_at -> Timestamptz,
+        is_email_verified -> Bool,
+    }
+}
+
+table! {
+    email_verification_tokens (email_verification_token_id) {
+        email_verification_token_id -> Uuid,
+        user_id -> Uuid,
+        email_verification_token -> Varchar,
+        email_verification_token_expires_at -> Timestamptz,
+        email_verification_tokens_created_at -> Timestamptz,
+    }
+}
+
+table! {
+    password_reset_tokens (password_reset_token_id) {
+        password_reset_token_id -> Uuid,
+        user_id -> Uuid,
+        password_reset_token -> Varchar,
+        password_reset_token_expires_at -> Timestamptz,
+        password_reset_token_created_at -> Timestamptz,
+    }
+}
+
+table! {
+    refresh_tokens (refresh_token_id) {
+        refresh_token_id -> Uuid,
+        user_id -> Uuid,
+        refresh_token -> Varchar,
+        refresh_token_issued_at -> Timestamptz,
+        refresh_token_expires_at -> Timestamptz,
+        refresh_token_revoked -> Bool,
     }
 }
 
@@ -27,4 +59,6 @@ pub struct User {
     pub user_created_at: DateTime<Utc>,
     #[diesel(sql_type = diesel::sql_types::Timestamptz)]
     pub user_updated_at: DateTime<Utc>,
+    #[diesel(sql_type = diesel::sql_types::Bool)]
+    pub is_email_verified: bool,
 }
