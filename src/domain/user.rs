8b446db1db5bd849acfe5b6517478d10764d93fd
Younghyun Ji <younghyun1@gmail.com>
2025-02-05 17:24:07 +0900

    separated schema file from domain defs

--- src/domain/user.rs
+++ src/domain/user.rs
@@ -1,50 +1,9 @@
 use chrono::{DateTime, Utc};
-use diesel::{prelude::Insertable, table, Queryable, QueryableByName};
+use diesel::{prelude::Insertable, Queryable, QueryableByName};
 use serde_derive::{Deserialize, Serialize};
 use uuid::Uuid;
 
-table! {
-    users (user_id) {
-        user_id -> Uuid,
-        user_name -> Varchar,
-        user_email -> Varchar,
-        user_password_hash -> Varchar,
-        user_created_at -> Timestamptz,
-        user_updated_at -> Timestamptz,
-        user_is_email_verified -> Bool,
-    }
-}
-
-table! {
-    email_verification_tokens (email_verification_token_id) {
-        email_verification_token_id -> Uuid,
-        user_id -> Uuid,
-        email_verification_token -> Uuid,
-        email_verification_token_expires_at -> Timestamptz,
-        email_verification_token_created_at -> Timestamptz,
-    }
-}
-
-table! {
-    password_reset_tokens (password_reset_token_id) {
-        password_reset_token_id -> Uuid,
-        user_id -> Uuid,
-        password_reset_token -> Uuid,
-        password_reset_token_expires_at -> Timestamptz,
-        password_reset_token_created_at -> Timestamptz,
-    }
-}
-
-table! {
-    refresh_tokens (refresh_token_id) {
-        refresh_token_id -> Uuid,
-        user_id -> Uuid,
-        refresh_token -> Uuid,
-        refresh_token_issued_at -> Timestamptz,
-        refresh_token_expires_at -> Timestamptz,
-        refresh_token_revoked -> Bool,
-    }
-}
+use crate::schema::{email_verification_tokens, users};
 
 #[derive(Serialize, Deserialize, QueryableByName, Queryable)]
 pub struct User {
