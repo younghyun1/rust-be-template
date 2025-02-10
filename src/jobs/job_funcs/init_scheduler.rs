e72a4eb8763f815a571ba343f4e349a14d0648a8
Younghyun Ji <younghyun1@gmail.com>
2025-02-10 17:15:49 +0900

    made the task init structure much more ergonomic without as much state arc cloning

--- src/jobs/job_funcs/init_scheduler.rs
+++ src/jobs/job_funcs/init_scheduler.rs
@@ -6,7 +6,7 @@ use crate::{
     init::state::ServerState,
     jobs::{
         auth::invalidate_sessions::invalidate_sessions,
-        job_funcs::every_minute::schedule_task_every_minute_at,
+        job_funcs::every_hour::schedule_task_every_hour_at,
     },
     // jobs::job_funcs::{
     //     every_minute::schedule_task_every_minute_at, every_second::schedule_task_every_second_at,
@@ -16,18 +16,11 @@ use crate::{
 pub async fn task_init(state: Arc<ServerState>) -> anyhow::Result<()> {
     info!("Task scheduler running...");
 
+    let coroutine_state = Arc::clone(&state);
     tokio::spawn(async move {
-        let state_1 = Arc::clone(&state);
-        let state_2 = Arc::clone(&state);
-        schedule_task_every_minute_at(
-            state_1,
-            move |_| {
-                let task_state = state_2.clone();
-                async move {
-                    // Clone inside the closure so that invalidate_sessions gets its own Arc copy.
-                    invalidate_sessions(Arc::clone(&task_state)).await;
-                }
-            },
+        schedule_task_every_hour_at(
+            coroutine_state,
+            move |state: Arc<ServerState>| async move { invalidate_sessions(state).await },
             String::from("INVALIDATE_EXPIRED_SESSIONS"),
             0,
             0,
