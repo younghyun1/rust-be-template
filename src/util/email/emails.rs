a288b1dc6534b2d8e9ce28ebb27e5f54edf2d5e6
Ji Younghyun <younghyun1@gmail.com>
2025-02-17 02:02:03 +0900

    db: refresh table unnecessary

---
+++ src/util/email/emails.rs
@@ -0,0 +1,17 @@
+pub const PASSWORD_RESET_EMAIL: &str = include_str!("./password_reset.html");
+
+pub struct PasswordResetEmail {
+    pub email: String,
+}
+
+impl PasswordResetEmail {
+    pub fn new() -> PasswordResetEmail {
+        PasswordResetEmail {
+            email: PASSWORD_RESET_EMAIL.to_string(),
+        }
+    }
+
+    pub fn set_link(&mut self, link: &str) {
+        self.email = self.email.replace("$1", link);
+    }
+}
