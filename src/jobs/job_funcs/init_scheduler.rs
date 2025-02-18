8cfff2e3e96eb470c22aff0660a9cf958df96fa3
Younghyun Ji <younghyun1@gmail.com>
2025-02-18 13:24:22 +0900

    feat: nonverified users purged daily

--- src/jobs/job_funcs/init_scheduler.rs
+++ src/jobs/job_funcs/init_scheduler.rs
@@ -5,8 +5,13 @@ use tracing::info;
 use crate::{
     init::state::ServerState,
     jobs::{
-        auth::invalidate_sessions::invalidate_sessions,
-        job_funcs::every_hour::schedule_task_every_hour_at,
+        auth::{
+            invalidate_sessions::invalidate_sessions,
+            purge_nonverified_users::purge_nonverified_users,
+        },
+        job_funcs::{
+            every_hour::schedule_task_every_hour_at, every_second::schedule_task_every_second_at,
+        },
     },
     // jobs::job_funcs::{
     //     every_minute::schedule_task_every_minute_at, every_second::schedule_task_every_second_at,
@@ -24,6 +29,20 @@ pub async fn task_init(state: Arc<ServerState>) -> anyhow::Result<()> {
                 invalidate_sessions(coroutine_state).await
             },
             String::from("INVALIDATE_EXPIRED_SESSIONS"),
+            30, // minutes
+            00, // seconds
+        )
+        .await
+    });
+
+    let coroutine_state = Arc::clone(&state);
+    tokio::spawn(async move {
+        schedule_task_every_second_at(
+            coroutine_state,
+            move |coroutine_state: Arc<ServerState>| async move {
+                purge_nonverified_users(coroutine_state).await
+            },
+            String::from("PURGE_NONVERIFIED_USERS"),
             30, // minutes
             00, // seconds
         )
