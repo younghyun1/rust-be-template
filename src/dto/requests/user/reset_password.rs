4267bc67487712dfebb2f32e865055dfb35c3128
지영현/전임 <younghyun.chi@genesisnest.com>
2025-02-20 13:38:37 +0900

    used password reset tokens are now marked as used

--- src/dto/requests/user/reset_password.rs
+++ src/dto/requests/user/reset_password.rs
@@ -2,6 +2,6 @@ use uuid::Uuid;
 
 #[derive(serde_derive::Deserialize)]
 pub struct ResetPasswordProcessRequest {
-    pub password_reset_token_id: Uuid,
+    pub password_reset_token: Uuid,
     pub new_password: String,
 }
