ad03353c29365b633f76322c568b1eed242c6663
Younghyun Ji <younghyun1@gmail.com>
2025-03-02 15:33:12 +0900

    feature: post update mechanism added if post_id exists in request

--- src/dto/requests/blog/submit_post_request.rs
+++ src/dto/requests/blog/submit_post_request.rs
@@ -1,7 +1,9 @@
 use serde_derive::Deserialize;
+use uuid::Uuid;
 
 #[derive(Deserialize)]
 pub struct SubmitPostRequest {
+    pub post_id: Option<Uuid>,
     pub post_title: String,
     pub post_content: String,
     pub post_tags: Vec<String>,
