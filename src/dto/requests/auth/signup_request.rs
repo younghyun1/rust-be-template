039ea3aa0a7053424f1ee8a4f2b4f4826821ef26
Younghyun Ji <younghyun1@gmail.com>
2025-03-04 16:40:59 +0900

    fix: db schema file and signup request fixed

--- src/dto/requests/auth/signup_request.rs
+++ src/dto/requests/auth/signup_request.rs
@@ -3,4 +3,7 @@ pub struct SignupRequest {
     pub user_name: String,
     pub user_email: String,
     pub user_password: String,
+    pub user_country: i32,
+    pub user_language: i32,
+    pub user_subdivision: Option<i32>,
 }
