962c6a016a23ff06f12f6fa58ffc08b51d0a79e3
Younghyun Ji <younghyun1@gmail.com>
2025-03-07 04:21:06 +0900

    added pkg-config; also added row sync

---
+++ src/handlers/countries/get_languages.rs
@@ -0,0 +1,11 @@
+use std::sync::Arc;
+
+use axum::{extract::State, response::IntoResponse};
+
+use crate::{errors::code_error::HandlerResponse, init::state::ServerState};
+
+pub async fn get_languages(
+    State(state): State<Arc<ServerState>>,
+) -> HandlerResponse<impl IntoResponse> {
+    Ok(())
+}
