d256a02b13933f221a754923406b8507071e0b00
Ji Younghyun <younghyun1@gmail.com>
2025-03-12 22:37:22 +0900

    feature: socketaddr

--- src/handlers/server/lookup_ip_loc.rs
+++ src/handlers/server/lookup_ip_loc.rs
@@ -1,6 +1,10 @@
 use std::{net::Ipv4Addr, sync::Arc};
 
-use axum::{Json, debug_handler, extract::State, response::IntoResponse};
+use axum::{
+    debug_handler,
+    extract::{Path, State},
+    response::IntoResponse,
+};
 
 use crate::{
     dto::responses::response_data::http_resp,
@@ -9,20 +13,14 @@ use crate::{
     util::{geographic::ip_info_lookup::IpInfo, time::now::tokio_now},
 };
 
-#[derive(serde_derive::Deserialize)]
-pub struct LookupIpLocationRequest {
-    ip_address: String,
-}
-
 #[debug_handler]
 pub async fn lookup_ip_location(
     State(state): State<Arc<ServerState>>,
-    Json(request): Json<LookupIpLocationRequest>,
+    Path(ip_address): Path<String>,
 ) -> HandlerResponse<impl IntoResponse> {
     let start = tokio_now();
 
-    let ip_address: Ipv4Addr = request
-        .ip_address
+    let ip_address: Ipv4Addr = ip_address
         .parse()
         .map_err(|e| code_err(CodeError::INVALID_IP_ADDRESS, e))?;
 
