2ecbee787d119a5439cda8ea6ee8ad2ec1572307
Ji Younghyun <younghyun1@gmail.com>
2025-03-17 23:13:18 +0900

    feature: email verification token

--- src/util/email/emails.rs
+++ src/util/email/emails.rs
@@ -33,3 +33,49 @@ impl PasswordResetEmail {
             .unwrap()
     }
 }
+
+pub struct ValidateEmailEmail {
+    pub email: String,
+}
+
+impl Default for ValidateEmailEmail {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+impl ValidateEmailEmail {
+    pub fn new() -> Self {
+        ValidateEmailEmail {
+            email: VALIDATE_EMAIL_EMAIL.to_string(),
+        }
+    }
+
+    pub fn set_fields(
+        mut self,
+        valid_until: chrono::DateTime<chrono::Utc>,
+        token_id: uuid::Uuid,
+    ) -> Self {
+        self.email = self
+            .email
+            .replace(
+                "$1",
+                &format!(
+                    "https://cyhdev.com/account/signup/validate-email?email_validation_token_id={}",
+                    token_id
+                ),
+            )
+            .replace("$2", &valid_until.to_string());
+        self
+    }
+
+    pub fn to_message(self, user_email: &str) -> lettre::Message {
+        lettre::Message::builder()
+            .from("Cyhdev Forums <donotreply@cyhdev.com>".parse().unwrap())
+            .to(user_email.parse().unwrap())
+            .subject("Validate Your Email")
+            .header(lettre::message::header::ContentType::TEXT_HTML)
+            .body(self.email)
+            .unwrap()
+    }
+}
