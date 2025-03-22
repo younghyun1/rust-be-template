589d6d545d99752085d9408db019b70b71892e5d
Younghyun Ji <younghyun1@gmail.com>
2025-03-22 20:19:10 +0900

    feature: user profile picture tables

--- migrations/2025-03-22-104939_profile_picture_history/up.sql
+++ migrations/2025-03-22-104939_profile_picture_history/up.sql
@@ -17,9 +17,11 @@ VALUES
 CREATE TABLE public.user_profile_pictures (
     user_profile_picture_id uuid DEFAULT uuid_generate_v4 () NOT NULL,
     user_id uuid NOT NULL,
-    user_profile_pictures_user_profile_pictures_created_at timestamptz DEFAULT now () NOT NULL,
-    user_profile_pictures_user_profile_pictures_updated_at timestamptz DEFAULT now () NOT NULL,
+    user_profile_picture_user_profile_pictures_created_at timestamptz DEFAULT now () NOT NULL,
+    user_profile_picture_user_profile_pictures_updated_at timestamptz DEFAULT now () NOT NULL,
     user_profile_picture_image_type int4 NOT NULL,
+    user_profile_pictures_is_on_cloud bool NOT NULL,
+    user_profile_picture_link varchar,
     CONSTRAINT user_profile_pictures_pkey PRIMARY KEY (user_profile_picture_id),
     CONSTRAINT fk_user_profile_pictures_user FOREIGN KEY (user_id) REFERENCES public.users (user_id),
     CONSTRAINT fk_user_profile_pictures_image_type FOREIGN KEY (user_profile_picture_image_type) REFERENCES public.user_profile_picture_image_types (image_type_id)
@@ -32,3 +34,5 @@ CREATE INDEX idx_user_profile_pictures_image_type ON public.user_profile_picture
 CREATE INDEX idx_user_profile_pictures_created_at ON public.user_profile_pictures (user_profile_pictures_created_at);
 
 CREATE INDEX idx_user_profile_pictures_updated_at ON public.user_profile_pictures (user_profile_pictures_updated_at);
+
+CREATE INDEX idx_user_profile_pictures_is_on_cloud ON public.user_profile_pictures (user_profile_pictures_is_on_cloud);
