93ed53fb3bd94f568cde16253dfcc967a139e2a9
Younghyun Ji <younghyun1@gmail.com>
2025-05-26 00:17:12 +0900

    feature: added build.rs for build-time behavior

---
+++ build.rs
@@ -0,0 +1,30 @@
+use std::fs;
+use std::fs::File;
+use std::io::Write;
+
+fn main() {
+    let build_time = chrono::Utc::now().to_rfc3339();
+
+    let cargo_lock = fs::read_to_string("Cargo.lock").expect("Cargo.lock not found");
+    let version = cargo_lock
+        .lines()
+        .collect::<Vec<_>>()
+        .windows(4)
+        .find(|window| {
+            window[0].trim() == "[[package]]"
+                && window[1].trim() == "name = \"axum\""
+                && window[2].trim().starts_with("version = ")
+        })
+        .and_then(|window| {
+            window[2]
+                .trim()
+                .strip_prefix("version = ")
+                .and_then(|v| v.trim_matches('"').into())
+        })
+        .map(|v: &str| v.to_string())
+        .unwrap_or_else(|| "unknown".to_string());
+
+    let mut file = File::create("src/build_info.rs").unwrap();
+    write!(file, "pub const BUILD_TIME: &str = \"{}\";\n", build_time).unwrap();
+    write!(file, "pub const AXUM_VERSION: &str = \"axum {}\";\n", version).unwrap();
+}
