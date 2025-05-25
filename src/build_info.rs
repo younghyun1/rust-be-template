9630ea293e49c09efd69f558ac49912c3bda981f
Ji Younghyun <younghyun1@gmail.com>
2025-05-26 01:11:43 +0900

    feature: clippy format and fix

--- src/build_info.rs
+++ src/build_info.rs
@@ -1,3 +1,3 @@
-pub const BUILD_TIME: &str = "2025-05-25T15:51:09.865674434+00:00";
+pub const BUILD_TIME: &str = "2025-05-25T16:05:12.165512429+00:00";
 pub const AXUM_VERSION: &str = "axum 0.8.4";
 pub const RUST_VERSION: &str = "cargo 1.89.0-nightly (056f5f4f3 2025-05-09)";
