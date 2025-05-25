46f1537f82388380b37021f99de9cd2a95009028
Younghyun Ji <younghyun1@gmail.com>
2025-05-26 00:52:47 +0900

    fix: voting

--- src/build_info.rs
+++ src/build_info.rs
@@ -1,3 +1,3 @@
-pub const BUILD_TIME: &str = "2025-05-25T15:47:00.548525497+00:00";
+pub const BUILD_TIME: &str = "2025-05-25T15:51:09.865674434+00:00";
 pub const AXUM_VERSION: &str = "axum 0.8.4";
-pub const RUST_VERSION: &str = "rustc cargo 1.89.0-nightly (056f5f4f3 2025-05-09)";
+pub const RUST_VERSION: &str = "cargo 1.89.0-nightly (056f5f4f3 2025-05-09)";
