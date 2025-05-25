dacff3a5be865f353cc0ef8d84e0e288cb00508a
Younghyun Ji <younghyun1@gmail.com>
2025-05-26 00:17:12 +0900

    feature: added build.rs for build-time behavior

--- src/dto/responses/auth/me_response.rs
+++ src/dto/responses/auth/me_response.rs
@@ -3,6 +3,8 @@ use serde_derive::Serialize;
 
 #[derive(Serialize)]
 pub struct MeResponse {
-    pub user_info: UserInfo,
+    pub user_info: Option<UserInfo>,
     pub user_profile_picture: Option<UserProfilePicture>,
+    pub build_time: &'static str,
+    pub axum_version: &'static str,
 }
