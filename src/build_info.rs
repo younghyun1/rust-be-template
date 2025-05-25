3e3b9976e2ae603935537a96b22eb14b335e1558
Younghyun Ji <younghyun1@gmail.com>
2025-05-26 00:38:15 +0900

    feature: static header for server info

--- src/build_info.rs
+++ src/build_info.rs
@@ -1,2 +1,2 @@
-pub const BUILD_TIME: &str = "2025-05-25T15:16:52.099282652+00:00";
+pub const BUILD_TIME: &str = "2025-05-25T15:37:57.297474080+00:00";
 pub const AXUM_VERSION: &str = "axum 0.8.4";
