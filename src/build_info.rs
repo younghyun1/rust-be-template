0efbc2a9b52556c71ede9a9a6aa17167b49cecc1
Younghyun Ji <younghyun1@gmail.com>
2025-06-19 13:30:19 +0900

    feature: fixed binary handling

--- src/build_info.rs
+++ src/build_info.rs
@@ -1,3 +1,3 @@
-pub const BUILD_TIME: &str = "2025-06-19T03:25:39.043202+00:00";
+pub const BUILD_TIME: &str = "2025-06-19T04:27:40.413323+00:00";
 pub const AXUM_VERSION: &str = "axum 0.8.4";
 pub const RUST_VERSION: &str = "cargo 1.89.0-nightly (fc1518ef0 2025-06-06)";
