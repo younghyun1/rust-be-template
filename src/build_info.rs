b3f94c995334ad16f50429f642406aac1818439f
Ji Younghyun <younghyun1@gmail.com>
2025-06-27 16:02:15 +0900

    sticking to take 1 for now; upgraded deps

--- src/build_info.rs
+++ src/build_info.rs
@@ -1,3 +1,3 @@
-pub const BUILD_TIME: &str = "2025-06-27T07:00:42.887750+00:00";
+pub const BUILD_TIME: &str = "2025-06-27T07:01:42.673111+00:00";
 pub const AXUM_VERSION: &str = "axum 0.8.4";
 pub const RUST_VERSION: &str = "cargo 1.90.0-nightly (409fed7dc 2025-06-23)";
