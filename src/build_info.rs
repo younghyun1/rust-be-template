add3af0e0e22543b6717f69c2283a05c508323ac
지영현/BE/GN <z308520@kia-partners.com>
2025-07-09 21:00:51 +0900

    feature: gzip -> zstd

--- src/build_info.rs
+++ src/build_info.rs
@@ -1,3 +1,3 @@
-pub const BUILD_TIME: &str = "2025-07-09T09:24:35.509876+00:00";
+pub const BUILD_TIME: &str = "2025-07-09T12:00:45.054842+00:00";
 pub const AXUM_VERSION: &str = "axum 0.8.4";
 pub const RUST_VERSION: &str = "cargo 1.90.0-nightly (930b4f62c 2025-06-28)";
