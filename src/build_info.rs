a1d2acc870977aba8a6ef975bef96e3a5ca9298a
Ji Younghyun <younghyun1@gmail.com>
2025-07-10 14:25:03 +0900

    feature: will embed FE assets on compile-time and serve them from RAM

--- src/build_info.rs
+++ src/build_info.rs
@@ -1,3 +1,3 @@
-pub const BUILD_TIME: &str = "2025-07-10T04:14:48.097529+00:00";
+pub const BUILD_TIME: &str = "2025-07-10T04:54:52.272849+00:00";
 pub const AXUM_VERSION: &str = "axum 0.8.4";
 pub const RUST_VERSION: &str = "cargo 1.90.0-nightly (930b4f62c 2025-06-28)";
