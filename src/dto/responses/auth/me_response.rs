68ba89d12e4e836c7ddf1e22e3c232cebabad337
지영현/BE/GN <z308520@kia-partners.com>
2025-07-17 17:31:16 +0900

    feature: modernized build.rs

--- src/dto/responses/auth/me_response.rs
+++ src/dto/responses/auth/me_response.rs
@@ -6,5 +6,5 @@ pub struct MeResponse {
     pub user_info: Option<UserInfo>,
     pub user_profile_picture: Option<UserProfilePicture>,
     pub build_time: &'static str,
-    pub axum_version: &'static str,
+    pub axum_version: String,
 }
