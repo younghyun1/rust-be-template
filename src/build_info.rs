bacb8c85ccea2072890d813c4f8a6fa9483adcc8
Ji Younghyun <younghyun1@gmail.com>
2025-07-17 10:56:04 +0900

    feature: added geo-ip DBs to .gitignore

--- src/build_info.rs
+++ src/build_info.rs
@@ -1,3 +1,3 @@
-pub const BUILD_TIME: &str = "2025-07-11T10:25:50.245576+00:00";
+pub const BUILD_TIME: &str = "2025-07-17T01:55:41.378488+00:00";
 pub const AXUM_VERSION: &str = "axum 0.8.4";
-pub const RUST_VERSION: &str = "cargo 1.90.0-nightly (930b4f62c 2025-06-28)";
+pub const RUST_VERSION: &str = "cargo 1.90.0-nightly (eabb4cd92 2025-07-09)";
