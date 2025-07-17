4fd90a68324405224302457b6ac692005024dfd1
지영현/BE/GN <z308520@kia-partners.com>
2025-07-17 11:35:09 +0900

    feature: removed all mentions of Ipv4Addr - Ipv6 ready now

--- src/build_info.rs
+++ src/build_info.rs
@@ -1,3 +1,3 @@
-pub const BUILD_TIME: &str = "2025-07-17T01:55:41.378488+00:00";
+pub const BUILD_TIME: &str = "2025-07-17T02:34:52.508757+00:00";
 pub const AXUM_VERSION: &str = "axum 0.8.4";
 pub const RUST_VERSION: &str = "cargo 1.90.0-nightly (eabb4cd92 2025-07-09)";
