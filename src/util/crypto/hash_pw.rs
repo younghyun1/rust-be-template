566766786a695910fb13ac586b35c2376c96719f
Ji Younghyun <younghyun1@gmail.com>
2025-10-17 02:57:37 -0600

    feature: replaced zstd with gzip for mac users; replaced rand 0.8 w. rand 0.9

--- src/util/crypto/hash_pw.rs
+++ src/util/crypto/hash_pw.rs
@@ -4,7 +4,7 @@ use argon2::{Argon2, PasswordHasher, password_hash::SaltString};
 pub async fn hash_pw(password: String) -> Result<String> {
     tokio::task::spawn_blocking(move || {
         let argon2 = Argon2::default();
-        let salt = SaltString::generate(&mut rand_08::rngs::OsRng);
+        let salt = SaltString::generate(&mut rand::rngs::OsRng);
         argon2
             .hash_password(password.as_bytes(), &salt)
             .map(|password_hash| password_hash.to_string())
