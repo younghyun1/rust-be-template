fd63082f49738c0a7456e67fa73df860e498250d
Ji Younghyun <younghyun1@gmail.com>
2025-11-30 02:14:13 -0700

    build: revised build script

--- build.sh
+++ build.sh
@@ -6,5 +6,3 @@ cd ../rust-be-template/
 cargo upgrade --incompatible
 cargo update
 cargo +nightly build -Z build-std=std,core,alloc,panic_unwind --release
-scp ./target/release/rust-be-template miniserver:~/dist/rust-be-template
-ssh miniserver "sudo systemctl restart rust-be-service"
