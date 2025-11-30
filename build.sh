9a07ef4ba487b38e04e36a754bee67f6a66c05f0
Ji Younghyun <younghyun1@gmail.com>
2025-11-30 02:05:14 -0700

    build: revised build script

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 cd ../solid-csr-spa-template/
 ./deploy_to_be.sh
+cd ../rust-be-template/
 cargo upgrade --incompatible
 cargo update
 cargo +nightly build -Z build-std=std,core,alloc,panic_unwind --release
