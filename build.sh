1cfacf2ec8886d208b659b27b0e979e4e82a9a4a
Ji Younghyun <younghyun1@gmail.com>
2025-12-03 00:11:49 -0700

    build: script fixed

--- build.sh
+++ build.sh
@@ -5,6 +5,7 @@ npm update
 npm install
 ./deploy_to_be.sh
 cd ../rust-be-template/
+rustup update
 cargo upgrade --incompatible
 cargo update
 cargo +nightly build -Z build-std=std,core,alloc,panic_unwind --release
